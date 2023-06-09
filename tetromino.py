28d4b00adea698d34e2384e6740eb98367f8698a
Nullblacc134 <136028420+Nullblacc134@users.noreply.github.com>
2023-06-09 17:15:20 -0400

    Update tetromino.py

--- tetromino.py
+++ tetromino.py
@@ -1,43 +1,21 @@
 import random
-
 from settings import *
-
-
 class Block(pg.sprite.Sprite):
     def __init__(self, tetromino, pos):
         self.tetromino = tetromino
         self.pos = vec(pos) + INIT_POS_OFFSET
         self.next_pos = vec(pos) + NEXT_POS_OFFSET
         self.alive = True
-
         super().__init__(tetromino.tetris.sprite_group)
         self.image = tetromino.image
         self.rect = self.image.get_rect()
 
-        self.sfx_image = self.image.copy()
-        self.sfx_image.set_alpha(110)
-        self.sfx_speed = random.uniform(0.2, 0.6)
-        self.sfx_cycles = random.randrange(6, 8)
-        self.cycle_counter = 0
 
-    def sfx_end_time(self):
-        if self.tetromino.tetris.app.anim_trigger:
-            self.cycle_counter += 1
-            if self.cycle_counter > self.sfx_cycles:
-                self.cycle_counter = 0
-                return True
 
-    def sfx_run(self):
-        self.image = self.sfx_image
-        self.pos.y -= self.sfx_speed
-        self.image = pg.transform.rotate(self.image, pg.time.get_ticks() * self.sfx_speed)
 
     def is_alive(self):
         if not self.alive:
-            if not self.sfx_end_time():
-                self.sfx_run()
-            else:
-                self.kill()
+            self.kill()
 
     def rotate(self, pivot_pos):
         translated = self.pos - pivot_pos
